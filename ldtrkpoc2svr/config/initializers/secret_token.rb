82e32c9763a2e62f45e0644aa360184e9eb47b1e
Shane Zatezalo <shane@lottadot.com>
2011-12-08 19:14:27 -0500

    initial version

---
+++ ldtrkpoc2svr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ldtrkpoc2svr::Application.config.secret_token = 'fc718b8505e3ee451633c65b6b766f46f02c26d7f0ef574660fe674718a710ef5b722bac87167fa04b5c85ccc347bd0dbeb15208c79fb007f9439eebd42949c3'
