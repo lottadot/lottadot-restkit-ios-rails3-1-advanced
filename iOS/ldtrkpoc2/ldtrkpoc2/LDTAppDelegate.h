368a82167b7df74c7bffe8fc7e43434acc97b512
Shane Zatezalo <shane@lottadot.com>
2011-12-08 21:15:35 -0500

    Revert "Revert "Added initial iOS Project""
    This reverts commit 9d64ddc667896dbfb3944c5281a25f3b36dc6b26.

---
+++ iOS/ldtrkpoc2/ldtrkpoc2/LDTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LDTAppDelegate.h
+//  ldtrkpoc2
+//
+//  Created by Shane Zatezalo on 12/8/11.
+//  Copyright (c) 2011 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/ldtrkpoc2/ldtrkpoc2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ldtrkpoc2
+//
+//  Created by Shane Zatezalo on 12/8/11.
+//  Copyright (c) 2011 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LDTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LDTAppDelegate class]));
+    }
+}
