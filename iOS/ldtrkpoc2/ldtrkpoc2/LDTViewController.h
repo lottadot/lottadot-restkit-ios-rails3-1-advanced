e407c6d805ea7b54efb9dcbd55daec072450c63e
Shane Zatezalo <shane@lottadot.com>
2011-12-08 21:09:08 -0500

    Added initial iOS Project
    * with Reskit via Static Library

---
+++ iOS/ldtrkpoc2/ldtrkpoc2/LDTViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDTViewController.h
+//  ldtrkpoc2
+//
+//  Created by Shane Zatezalo on 12/8/11.
+//  Copyright (c) 2011 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDTViewController : UIViewController
+
+@end
---
+++ iOS/ldtrkpoc2/ldtrkpoc2Tests/ldtrkpoc2Tests.h
@@ -0,0 +1,13 @@
+//
+//  ldtrkpoc2Tests.h
+//  ldtrkpoc2Tests
+//
+//  Created by Shane Zatezalo on 12/8/11.
+//  Copyright (c) 2011 Personal. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ldtrkpoc2Tests : SenTestCase
+
+@end
